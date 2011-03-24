6873bc5536d586a4326ed425f44dc8c940a47bcd
Michael Koval <koval.michael@gmail.com>
2011-03-24 03:11:54 -0400

    wiimote: added package

---
+++ wiimote/msg_gen/lisp/_package.lisp
@@ -0,0 +1,15 @@
+(cl:defpackage wiimote-msg
+  (:use )
+  (:export
+   "<IRSOURCEINFO>"
+   "IRSOURCEINFO"
+   "<LEDCONTROL>"
+   "LEDCONTROL"
+   "<RUMBLECONTROL>"
+   "RUMBLECONTROL"
+   "<STATE>"
+   "STATE"
+   "<TIMEDSWITCH>"
+   "TIMEDSWITCH"
+  ))
+
---
+++ wiimote/msg_gen/lisp/_package_LEDControl.lisp
@@ -0,0 +1,4 @@
+(cl:in-package wiimote-msg)
+(cl:export '(TIMED_SWITCH_ARRAY-VAL
+          TIMED_SWITCH_ARRAY
+))
