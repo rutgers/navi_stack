a13eef138dc866a770b84cc0ea1da849a9558182
Michael Koval <koval.michael@gmail.com>
2011-04-11 19:47:34 -0400

    stereo_od: switched to nodelet

--- stereo_od/src/od_node.cpp
+++ stereo_od/src/od_node.cpp
@@ -1,46 +1,60 @@
 #include <cmath>
-#include <ros/ros.h>
+#include <vector>
 
 #include <boost/foreach.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/incremental_components.hpp>
 #include <boost/pending/disjoint_sets.hpp>
 
+#include <ros/ros.h>
 #include <image_geometry/pinhole_camera_model.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <pluginlib/class_list_macros.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/point_types.h>
 
-namespace mf = message_filters;
+#include "od_node.hpp"
 
-using image_geometry::PinholeCameraModel;
 using sensor_msgs::CameraInfo;
 using sensor_msgs::PointCloud2;
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
+PLUGINLIB_DECLARE_CLASS(stereo_od, od_nodelet, od_node::StereoODNodelet, nodelet::Nodelet)
 
-static int    m_pmin;
-static double m_dmax;
-static double m_hmin;
-static double m_hmax;
-static double m_theta;
-static PinholeCameraModel m_model;
-static float  float_nan = std::numeric_limits<float>::quiet_NaN();
+namespace od_node {
 
-static ros::Publisher  m_pub_pts;
+void StereoODNodelet::onInit(void)
+{
+	namespace mf = message_filters;
 
-float dist(pcl::PointXYZ const &pt1, pcl::PointXYZ const &pt2)
+	ros::NodeHandle nh      = getNodeHandle();
+	ros::NodeHandle nh_priv = getPrivateNodeHandle();
+
+	nh_priv.param<int>("points_min",      m_pmin,  25);
+	nh_priv.param<double>("distance_max", m_dmax,  5.0);
+	nh_priv.param<double>("height_min",   m_hmin,  0.3);
+	nh_priv.param<double>("height_max",   m_hmax,  2.0);
+	nh_priv.param<double>("theta",        m_theta, M_PI / 4);
+
+	m_pub_pts = nh.advertise<PointCloud2>("obstacle_points", 10);
+
+	mf::Subscriber<PointCloudXYZ> sub_pts(nh, "points", 1);
+	mf::Subscriber<CameraInfo>    sub_info(nh, "camera_info", 1);
+	mf::TimeSynchronizer<PointCloudXYZ, CameraInfo> sub_sync(sub_pts, sub_info, 10);
+	sub_sync.registerCallback(&StereoODNodelet::Callback, this);
+}
+
+float StereoODNodelet::Distance(pcl::PointXYZ const &pt1, pcl::PointXYZ const &pt2)
 {
 	return sqrt(pow(pt2.x - pt1.x, 2) + pow(pt2.y - pt1.y, 2) + pow(pt2.z - pt1.z, 2));
 }
 
-void FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst,
-                   double hmin, double hmax, double flen, double theta)
+void StereoODNodelet::FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst)
 {
+	float float_nan = std::numeric_limits<float>::quiet_NaN();
+
 	// Ranks is an associative data structure that stores the size of each
 	// connected component. Parents stores the relationship between nodes for
 	// use by path compression. Using raw arrays is more efficient than using
@@ -56,6 +70,7 @@ void FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst,
 
 	float const sin_theta = sin(m_theta);
 	float const tan_theta = tan(m_theta);
+	float const f_len     = m_model.fy();
 
 	dst.points.resize(src.width * src.height);
 	dst.width    = src.width;
@@ -80,7 +95,7 @@ void FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst,
 		// Project the cone above point P1 into the image as a trapezoid to
 		// reduce the search space for points inside the cone. This reduces the
 		// runtime of the algorithm from O(N^2) to O(K*N)
-		int cone_height = m_hmax * pt1_src.z / flen;
+		int cone_height = m_hmax * pt1_src.z / f_len;
 		if (y0 - cone_height < 0) {
 			cone_height = y0;
 		}
@@ -99,10 +114,9 @@ void FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst,
 				if (isnan(pt2_src.x) || isnan(pt2_src.y) || isnan(pt2_src.z)) continue;
 				if (pt2_src.z > m_dmax) continue;
 
-				// FIXME: height check may be redundant
 				float height = fabs(pt2_src.y - pt1_src.y);
-				float angle  = height / dist(pt1_src, pt2_src);
-				bool valid_height = hmin <= height && height <= hmax;
+				float angle  = height / Distance(pt1_src, pt2_src);
+				bool valid_height = m_hmin <= height && height <= m_hmax;
 				bool valid_angle  = angle >= sin_theta;
 
 				if (valid_height && valid_angle) {
@@ -135,44 +149,19 @@ void FindObstacles(PointCloudXYZ const &src, PointCloudXYZ &dst,
 	dst.is_dense = false;
 }
 
-void Callback(PointCloudXYZ::ConstPtr const &msg_pts, CameraInfo::ConstPtr const &msg_info)
+void StereoODNodelet::Callback(PointCloudXYZ::ConstPtr const &msg_pts, CameraInfo::ConstPtr const &msg_info)
 {
-	// Extract the camera's focal length from the CameraInfo message. Because
-	// we are reprojecting a vertical distance, we can safely ignore fx().
 	m_model.fromCameraInfo(msg_info);
-	double flen = m_model.fy();
 
 	PointCloudXYZ obstacles;
-	FindObstacles(*msg_pts, obstacles, m_hmin, m_hmax, flen, m_theta);
+	FindObstacles(*msg_pts, obstacles);
 
 	PointCloud2 msg_obstacles;
 	pcl::toROSMsg(obstacles, msg_obstacles);
 
+	// FIXME: Publish as a shared pointer for zero-copy.
 	msg_obstacles.header.stamp    = msg_pts->header.stamp;
 	msg_obstacles.header.frame_id = msg_pts->header.frame_id;
 	m_pub_pts.publish(msg_obstacles);
 }
-
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "od_node");
-
-	ros::NodeHandle nh;
-	ros::NodeHandle nh_priv("~");
-
-	nh_priv.param<int>("points_min",      m_pmin,  25);
-	nh_priv.param<double>("distance_max", m_dmax,  5.0);
-	nh_priv.param<double>("height_min",   m_hmin,  0.3);
-	nh_priv.param<double>("height_max",   m_hmax,  2.0);
-	nh_priv.param<double>("theta",        m_theta, M_PI / 4);
-
-	mf::Subscriber<PointCloudXYZ> sub_pts(nh, "points", 1);
-	mf::Subscriber<CameraInfo>    sub_info(nh, "camera_info", 1);
-	mf::TimeSynchronizer<PointCloudXYZ, CameraInfo> sub_sync(sub_pts, sub_info, 10);
-	sub_sync.registerCallback(&Callback);
-
-	m_pub_pts = nh.advertise<PointCloud2>("obstacle_points", 10);
-
-	ros::spin();
-	return 0;
-}
+};
