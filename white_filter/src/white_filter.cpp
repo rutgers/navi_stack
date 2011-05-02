babe1cfd25bdb24afe658ae1eb5e235c38f1faeb
Michael Koval <koval.michael@gmail.com>
2011-05-01 21:02:09 -0400

    white_filter: added blue hack for capstone demo

--- white_filter/src/white_filter.cpp
+++ white_filter/src/white_filter.cpp
@@ -18,30 +18,22 @@ void WhiteNodelet::onInit(void)
 	ros::NodeHandle &nh      = getNodeHandle();
 	ros::NodeHandle &nh_priv = getPrivateNodeHandle();
 
+	nh_priv.param<bool>("use_blue", m_use_blue, false);
 	nh_priv.param<bool>("use_low",  m_use_low,  true);
 	nh_priv.param<bool>("use_high", m_use_high, true);
 	nh_priv.param<int>("threshold_hue", m_threshold_hue, 180);
 	nh_priv.param<int>("threshold_sat", m_threshold_sat, 127);
+
+	nh_priv.param<int>("blue_hue", m_blue_hue, 127);
+	nh_priv.param<int>("blue_sat", m_blue_sat, 127);
 
 	m_it = boost::make_shared<image_transport::ImageTransport>(nh);
 	m_pub = m_it->advertise("white", 1);
 	m_sub = m_it->subscribe("image", 1, &WhiteNodelet::Callback, this);
 }
 
-void WhiteNodelet::Callback(sensor_msgs::Image::ConstPtr const &msg_img)
+void WhiteNodelet::FilterWhite(cv::Mat src, cv::Mat &dst)
 {
-	namespace enc = sensor_msgs::image_encodings;
-
-	// Convert the message to the OpenCV datatype without copying it.
-	cv::Mat src;
-	try {
-		cv_bridge::CvImageConstPtr src_tmp = cv_bridge::toCvShare(msg_img, enc::BGR8);
-		src = src_tmp->image;
-	} catch (cv_bridge::Exception const &e) {
-		ROS_WARN_THROTTLE(10, "unable to parse image message");
-		return;
-	}
-
 	// Convert the image into the HSV color space. By most ignoring pixel value,
 	// this algorithm is made much more robust to changes in ambiant lighting.
 	std::vector<cv::Mat> chans(3);
@@ -73,15 +65,53 @@ void WhiteNodelet::Callback(sensor_msgs::Image::ConstPtr const &msg_img)
 	if (m_use_high) cv::max(white, white_hi, white);
 
 	// Use this binary "white" indicator to mask the value image.
-	cv::Mat whiteness;
-	cv::min(val, white, whiteness);
+	cv::min(val, white, dst);
+}
+
+void WhiteNodelet::FilterBlue(cv::Mat src, cv::Mat &dst)
+{
+	std::vector<cv::Mat> chans(3);
+	cv::Mat hsv;
+	cv::cvtColor(src, hsv, CV_BGR2HSV);
+	cv::split(hsv, chans);
+	cv::Mat &hue = chans[0];
+	cv::Mat &sat = chans[1];
+	cv::Mat &val = chans[2];
+
+	cv::Mat diff_hue = cv::abs(hue - m_blue_hue);
+	cv::Mat diff_sat = cv::abs(sat - m_blue_sat);
+	cv::Mat diff     = diff_hue + diff_sat;
+
+	dst = 255 - diff;
+}
+
+void WhiteNodelet::Callback(sensor_msgs::Image::ConstPtr const &msg_img)
+{
+	namespace enc = sensor_msgs::image_encodings;
+
+	cv::Mat src, dst;
+
+	// Convert the message to the OpenCV datatype without copying it.
+	try {
+		cv_bridge::CvImageConstPtr src_tmp = cv_bridge::toCvShare(msg_img, enc::BGR8);
+		src = src_tmp->image;
+	} catch (cv_bridge::Exception const &e) {
+		ROS_WARN_THROTTLE(10, "unable to parse image message");
+		return;
+	}
+
+	if (m_use_blue) {
+		FilterBlue(src, dst);
+	} else {
+		FilterWhite(src, dst);
+	}
 
 	// Convert the OpenCV data to an output message without copying.
-	// FIXME: Just publishing saturation as a temporary stopgap.
 	cv_bridge::CvImage msg_white;
 	msg_white.header   = msg_img->header;
 	msg_white.encoding = enc::MONO8;
-	msg_white.image    = 255 - sat;
+	msg_white.image    = dst;
 	m_pub.publish(msg_white.toImageMsg());
 }
+
 };
--- white_filter/src/white_filter.hpp
+++ white_filter/src/white_filter.hpp
@@ -1,6 +1,7 @@
 #ifndef WHITE_FILTER_HPP_
 #define WHITE_FILTER_HPP_
 
+#include <opencv/cv.h>
 #include <image_transport/image_transport.h>
 #include <nodelet/nodelet.h>
 #include <sensor_msgs/Image.h>
@@ -12,6 +13,8 @@ public:
 	virtual void onInit(void);
 
 	void Callback(sensor_msgs::Image::ConstPtr const &ptr);
+	void FilterBlue(cv::Mat src, cv::Mat &dst);
+	void FilterWhite(cv::Mat src, cv::Mat &dst);
 
 private:
 	boost::shared_ptr<image_transport::ImageTransport> m_it;
@@ -22,6 +25,10 @@ private:
 	bool m_use_high;
 	int m_threshold_hue;
 	int m_threshold_sat;
+
+	bool m_use_blue;
+	int m_blue_hue;
+	int m_blue_sat;
 };
 };
 #endif
