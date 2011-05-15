d92fe3995252355750501eb7743da5c29fe48390
Michael Koval <koval.michael@gmail.com>
2011-05-15 19:22:40 -0400

    white_filter: works; output sucks and is too slow

--- white_filter/nodes/histogram_node.py
+++ white_filter/nodes/histogram_node.py
@@ -47,7 +47,8 @@ class HistogramNode:
 
 		# Load training data from a CSV file.
 		train_path = rospy.get_param('~train_path')
-		train_data = numpy.genfromtxt(train_path, delimiter=',', comments='@', dtype=float)
+		train_data = numpy.genfromtxt(train_path, delimiter=',', comments='@', dtype=numpy.uint8)
+		print([ train_data ])
 
 		# Build the HS-histogram of both positive and negative examples.
 		pos = train_data[train_data[:,CH_LABEL] == 1, :]
@@ -64,7 +65,7 @@ class HistogramNode:
 		self.pub = rospy.Publisher('white', sensor_msgs.msg.Image)
 
 	def LoadHistogram(self, samples, channels, bins, ranges, factor):
-		ch_numpy = [ samples[:, ch:(ch + 1)] / 255.0 for ch in channels ]
+		ch_numpy = [ samples[:, ch:(ch + 1)] for ch in channels ]
 		ch_cvmat = map(cv.fromarray, ch_numpy)
 		ch_image = map(cv.GetImage,  ch_cvmat)
 
@@ -75,10 +76,10 @@ class HistogramNode:
 		return histogram
 
 	def TransformImage(self, bgr, hsv, channels, size, method):
-		assert(bgr.type == cv.CV_32FC3)
-		assert(hsv.type == cv.CV_32FC3)
+		assert(bgr.type == cv.CV_8UC3)
+		assert(hsv.type == cv.CV_8UC3)
 
-		chs_all = [ cv.CreateMat(hsv.rows, hsv.cols, cv.CV_32FC1) for i in range(0, 6) ]
+		chs_all = [ cv.CreateMat(hsv.rows, hsv.cols, cv.CV_8UC1) for i in range(0, 6) ]
 		cv.Split(bgr, chs_all[CH_BLUE], chs_all[CH_GREEN], chs_all[CH_RED], None)
 		cv.Split(hsv, chs_all[CH_HUE],  chs_all[CH_SAT],   chs_all[CH_VAL], None)
 		chs = [ cv.GetImage(chs_all[ch]) for ch in channels ]
@@ -87,33 +88,28 @@ class HistogramNode:
 		dst_neg = cv.CreateMat(hsv.rows - size[1] + 1, hsv.cols - size[0] + 1, cv.CV_32FC1)
 		cv.CalcBackProjectPatch(chs, dst_pos, size, self.hist_pos, method, 1.0)
 		cv.CalcBackProjectPatch(chs, dst_neg, size, self.hist_neg, method, 1.0)
-
-
 		return (dst_pos, dst_neg)
 
 	def ImageCallback(self, msg_img):
 		# Convert the ROS message to a floating-point image in HSV-space.
-		bgr_8u  = self.bridge.imgmsg_to_cv(msg_img, 'bgr8')
-		hsv_8u  = cv.CreateMat(bgr_8u.rows, bgr_8u.cols, cv.CV_8UC3)
-		bgr_32f = cv.CreateMat(bgr_8u.rows, bgr_8u.cols, cv.CV_32FC3)
-		hsv_32f = cv.CreateMat(bgr_8u.rows, bgr_8u.cols, cv.CV_32FC3)
-
-		cv.CvtColor(bgr_8u, hsv_8u, cv.CV_BGR2HSV)
-		cv.ConvertScale(bgr_8u, bgr_32f, 1.0 / 255)
-		cv.ConvertScale(hsv_8u, hsv_32f, 1.0 / 255)
+		bgr = self.bridge.imgmsg_to_cv(msg_img, 'bgr8')
+		hsv = cv.CreateMat(bgr.rows, bgr.cols, cv.CV_8UC3)
+		cv.CvtColor(bgr, hsv, cv.CV_BGR2HSV)
 
 		# Do the color space transformation.
-		dst_pos, dst_neg = self.TransformImage(bgr_32f, hsv_32f, self.channels, self.window, self.method)
+		dst_pos, dst_neg = self.TransformImage(bgr, hsv, self.channels, self.window, self.method)
 
 		# DEBUG: Normalization for rendering.
+		"""
 		val_min, val_max, loc_min, loc_max = cv.MinMaxLoc(dst_pos)
 		print('min = {0}, max = {1}'.format(val_min, val_max))
-		dst_32f = (dst_pos - val_min) / (val_max - val_min)
-		dst_8u  = cv.CreateMat(dst_pos.rows, dst_pos.cols, cv.CV_8UC1)
-		cv.ConvertScale(dst_32f, dst_8u, 255.0)
+		"""
+
+		dst = cv.CreateMat(dst_pos.rows, dst_pos.cols, cv.CV_8UC1)
+		cv.ConvertScale(dst_neg, dst, 255.0)
 
 		# Publish the positive response; ignore the negative response.
-		msg_out = self.bridge.cv_to_imgmsg(dst_8u, 'mono8')
+		msg_out = self.bridge.cv_to_imgmsg(dst, 'mono8')
 		msg_out.header.stamp    = msg_img.header.stamp
 		msg_out.header.frame_id = msg_img.header.frame_id
 		self.pub.publish(msg_out)
