031825fe68410ee649085ee83c622c987380f1ed
Michael Koval <koval.michael@gmail.com>
2011-05-15 03:20:27 -0400

    white_filter: improved data collection scripts

--- white_filter/scripts/label.py
+++ white_filter/scripts/label.py
@@ -20,21 +20,21 @@ except IndexError:
 	sys.exit(1)
 
 writer   = csv.writer(sys.stdout)
-img_clr = cv.LoadImageM(file_clr, cv.CV_LOAD_IMAGE_COLOR)
+img_rgb = cv.LoadImageM(file_clr, cv.CV_LOAD_IMAGE_COLOR)
 img_tru = cv.LoadImageM(file_tru, cv.CV_LOAD_IMAGE_GRAYSCALE)
 
-img_hsv = cv.CreateMat(img_clr.rows, img_clr.cols, cv.CV_8UC3)
-cv.CvtColor(img_clr, img_hsv, cv.CV_RGB2HSV)
+img_hsv = cv.CreateMat(img_rgb.rows, img_rgb.cols, cv.CV_8UC3)
+cv.CvtColor(img_rgb, img_hsv, cv.CV_RGB2HSV)
 
-assert(img_clr.rows == img_tru.rows)
-assert(img_clr.cols == img_tru.cols)
+assert(img_rgb.rows == img_tru.rows)
+assert(img_rgb.cols == img_tru.cols)
 
 # Build a list of all the data in-memory.
-pixels = itertools.product(range(0, img_clr.rows), range(0, img_clr.cols))
+pixels = itertools.product(range(0, img_rgb.rows), range(0, img_rgb.cols))
 labels = { 0: [ ], 1: [ ] }
 
 for (y, x) in pixels:
-	rgb   = map(int, img_clr[y, x])
+	rgb   = map(int, img_rgb[y, x])
 	hsv   = map(int, img_hsv[y, x])
 	label = int(int(img_tru[y, x]) > 127)
 	labels[label].append(rgb + hsv + [ label ])
--- white_filter/scripts/label.sh
+++ white_filter/scripts/label.sh
@@ -12,6 +12,20 @@ path_tru="$1/truth"
 names=`ls "$path_clr"`
 temp=`mktemp`
 
+cat > "$temp" <<HEADER
+@RELATION lines
+
+@ATTRIBUTE red   NUMERIC
+@ATTRIBUTE green NUMERIC
+@ATTRIBUTE blue  NUMERIC
+@ATTRIBUTE hue   NUMERIC
+@ATTRIBUTE sat   NUMERIC
+@ATTRIBUTE val   NUMERIC
+@ATTRIBUTE line  {0,1}
+
+@DATA
+HEADER
+
 for name in $names; do
 	rosrun white_filter label.py "$path_clr/$name" "$path_tru/$name" >> "$temp"
 done
