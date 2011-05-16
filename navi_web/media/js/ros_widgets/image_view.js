232ac7052c19bb887c9b5664e6053ebfeda5b51f
Adam Stambler <adasta@gmail.com>
2011-05-15 22:50:26 -0400

    dynamic display of any widgets / topic using /widget/topic_name

--- navi_web/media/js/ros_widgets/image_view.js
+++ navi_web/media/js/ros_widgets/image_view.js
@@ -1,45 +1,46 @@
-function image_view (div_id, pix_width, pix_height){
-	this.lock = false;
-	this.div_id = div_id
-	this.pix_width = pix_width
-	this.pix_height = pix_height
+image_view = function(connection, domObj){
+	widget = new RosWidget(connection, domObj)	
 	
-	// Setup Div element for image_view
-	this.div = document.getElementById(div_id)
-	this.canvas = document.createElement('canvas');
-	this.div.appendChild(this.canvas)
+	widget.init = function(){
+		
+		log('init img view')
+		this.pix_width = domObj.getAttribute('pixel_width')
+		if (! this.pix_width) {
+			this.pix_width = 320
+		}
+		this.pix_height = domObj.getAttribute('pixel_height')
+		if (! this.pix_height){
+			this.pix_height= 240
+		}
+	
+		// Setup Div element for image_view
+		var canvas = document.createElement('canvas');
+		this.canvas = canvas
+		this.domObj.appendChild(this.canvas)
+		this.canvas.width = this.pix_width
+		this.canvas.height= this.pix_height
 
-	this.canvas.width = pix_width
-	this.canvas.height= pix_height
-	
-	var ctx = this.canvas.getContext('2d');
-			
-	//Add in some default image to show where the image_view is				
-	ctx.fillStyle = "rgb(200,200,200)";
-	ctx.fillRect (0, 0, pix_width, pix_height);
-	this.lock=false
-	
-	this.canvas.onLoad = function(){
-		this.lock= false
-	}			
-    this.handler=function(msg){
-		if (this.lock) return;
+		var ctx = this.canvas.getContext('2d');
 				
-		this.lock = true;
+		//Add in some default image to show where the image_view is				
+		ctx.fillStyle = "rgb(200,200,200)";
+		ctx.fillRect (0, 0, this.pix_width, this.pix_height);	
+		
+		
+		this.connection.Subscribe(this.topic, this.handler)
+    }
+
+	widget.handler=function(msg){
+		var ctx = widget.canvas.getContext('2d');
 		img = new Image();
 		img.onload = function(){
-		ctx.clearRect(0,0,this.pix_width,this.pix_height);
-		ctx.save();
-		ctx.scale(5,5);
-		ctx.drawImage(img,0,0);
-		ctx.restore();
-			};
-		//#log(msg.uri)
-		
-		this.lock=false;
+			ctx.clearRect(0,0,this.pix_width,this.pix_height);
+			ctx.save();
+			ctx.scale(2,2);
+			ctx.drawImage(img,0,0);
+			ctx.restore();
+			};		
 		img.src = msg.uri;
-
-    };
-
-    
-};
+	}
+	return widget
+}
