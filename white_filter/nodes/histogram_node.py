bccbbba2d5053ee186c68e92bb164cafb8192ce3
Michael Koval <koval.michael@gmail.com>
2011-05-18 16:31:46 -0400

    white_filter: split into multiple nodes

--- white_filter/nodes/histogram_node.py
+++ white_filter/nodes/histogram_node.py
@@ -14,9 +14,9 @@ import numpy
 import sys
 
 
-CH_RED   = 0
+CH_BLUE  = 0
 CH_GREEN = 1
-CH_BLUE  = 2
+CH_RED   = 2
 CH_HUE   = 3
 CH_SAT   = 4
 CH_VAL   = 5
@@ -27,37 +27,35 @@ class HistogramNode:
 		self.bridge = cv_bridge.CvBridge()
 
 		# Histogram parameters.
-		bins_hue = rospy.get_param('~bins_hue', 5)
+		bins_hue = rospy.get_param('~bins_hue', 10)
 		min_hue  = rospy.get_param('~min_hue', 0)
 		max_hue  = rospy.get_param('~max_hue', 255)
 
-		bins_sat = rospy.get_param('~bins_sat', 5)
+		bins_sat = rospy.get_param('~bins_sat', 10)
 		min_sat  = rospy.get_param('~min_sat', 0)
 		max_sat  = rospy.get_param('~max_sat', 255)
+		self.ker_size = rospy.get_param('~kernel_size', 3)
 
-		bins          = [ bins_hue, bins_sat ]
-		ranges        = [ (min_hue, max_hue), (min_sat, max_sat) ]
-		self.channels = [ CH_HUE, CH_SAT ]
+		self.bins     = [ bins_hue, bins_sat ]
+		self.ranges   = [ (min_hue, max_hue), (min_sat, max_sat) ]
+		self.channels = [ CH_HUE, CH_SAT, CH_VAL ]
 
 		# Histogram matching parameters.
-		width  = rospy.get_param('~window_width',  10)
-		height = rospy.get_param('~window_height', 10)
+		width  = rospy.get_param('~window_width',  20)
+		height = rospy.get_param('~window_height', 20)
 		self.window = (width, height)
 		self.method = cv.CV_COMP_CORREL
 
 		# Load training data from a CSV file.
 		train_path = rospy.get_param('~train_path')
 		train_data = numpy.genfromtxt(train_path, delimiter=',', comments='@', dtype=numpy.uint8)
+
 		# Build the HS-histogram of both positive and negative examples.
 		pos = train_data[train_data[:,CH_LABEL] == 1, :]
-		neg = train_data[train_data[:,CH_LABEL] == 0, :]
-		self.hist_pos = self.LoadHistogram(pos, self.channels, bins, ranges, 1.0)
-		self.hist_neg = self.LoadHistogram(neg, self.channels, bins, ranges, 1.0)
+		self.hist_pos = self.LoadHistogram(pos, self.channels, self.bins, self.ranges, 1.0)
 
 		val_min, val_max, loc_min, loc_max = cv.GetMinMaxHistValue(self.hist_pos)
-		rospy.loginfo('positive histogram counts is [{0}, {1}]'.format(val_min, val_max))
-		val_min, val_max, loc_min, loc_max = cv.GetMinMaxHistValue(self.hist_neg)
-		rospy.loginfo('negative histogram counts is [{0}, {1}]'.format(val_min, val_max))
+		rospy.loginfo('histogram counts are in range [{0}, {1}]'.format(val_min, val_max))
 
 		self.sub = rospy.Subscriber('image', sensor_msgs.msg.Image, self.ImageCallback)
 		self.pub = rospy.Publisher('white', sensor_msgs.msg.Image)
@@ -69,7 +67,6 @@ class HistogramNode:
 
 		histogram = cv.CreateHist(bins, cv.CV_HIST_ARRAY, ranges, True)
 		cv.CalcHist(ch_image, histogram, False)
-		print(histogram)
 		cv.NormalizeHist(histogram, factor)
 		return histogram
 
@@ -82,27 +79,27 @@ class HistogramNode:
 		cv.Split(hsv, chs_all[CH_HUE],  chs_all[CH_SAT],   chs_all[CH_VAL], None)
 		chs = [ cv.GetImage(chs_all[ch]) for ch in channels ]
 
-		dst_pos = cv.CreateMat(hsv.rows - size[1] + 1, hsv.cols - size[0] + 1, cv.CV_32FC1)
-		dst_neg = cv.CreateMat(hsv.rows - size[1] + 1, hsv.cols - size[0] + 1, cv.CV_32FC1)
-		cv.CalcBackProjectPatch(chs, dst_pos, size, self.hist_pos, method, 1.0)
-		cv.CalcBackProjectPatch(chs, dst_neg, size, self.hist_neg, method, 1.0)
-		return (dst_pos, dst_neg)
+		dst = cv.CreateMat(hsv.rows - size[1] + 1, hsv.cols - size[0] + 1, cv.CV_32FC1)
+		cv.CalcBackProjectPatch(chs, dst, size, self.hist_pos, method, 1.0)
+		return dst
 
 	def ImageCallback(self, msg_img):
 		# Convert the ROS message to a floating-point image in HSV-space.
 		bgr = self.bridge.imgmsg_to_cv(msg_img, 'bgr8')
+
+		if self.ker_size > 0:
+			cv.Smooth(bgr, bgr, cv.CV_GAUSSIAN, self.ker_size, self.ker_size)
+
 		hsv = cv.CreateMat(bgr.rows, bgr.cols, cv.CV_8UC3)
 		cv.CvtColor(bgr, hsv, cv.CV_BGR2HSV)
 
 		# Do the color space transformation.
-		dst_pos, dst_neg = self.TransformImage(bgr, hsv, self.channels, self.window, self.method)
-
-		# DEBUG: Normalization for rendering.
-		dst = cv.CreateMat(dst_pos.rows, dst_pos.cols, cv.CV_8UC1)
-		cv.ConvertScale(dst_pos, dst, 255.0)
+		dst_32f = self.TransformImage(bgr, hsv, self.channels, self.window, self.method)
+		dst_8u  = cv.CreateMat(dst_32f.rows, dst_32f.cols, cv.CV_8UC1)
+		cv.ConvertScale(dst_32f, dst_8u, 255.0)
 
 		# Publish the positive response; ignore the negative response.
-		msg_out = self.bridge.cv_to_imgmsg(dst, 'mono8')
+		msg_out = self.bridge.cv_to_imgmsg(dst_8u, 'mono8')
 		msg_out.header.stamp    = msg_img.header.stamp
 		msg_out.header.frame_id = msg_img.header.frame_id
 		self.pub.publish(msg_out)
---
+++ white_filter/src/ml_nodelet.cpp
@@ -0,0 +1,145 @@
+#include <fstream>
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <pluginlib/class_list_macros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv/cv.h>
+
+#include "csv.hpp"
+#include "ml_nodelet.hpp"
+
+PLUGINLIB_DECLARE_CLASS(white_filter, ml_nodelet, white_filter::MLNodelet, nodelet::Nodelet)
+
+namespace white_filter {
+
+void MLNodelet::onInit(void)
+{
+	ros::NodeHandle &nh      = getNodeHandle();
+	ros::NodeHandle &nh_priv = getPrivateNodeHandle();
+
+	int n_max;
+	std::string path, delim;
+	nh_priv.param<int>("kernel_size", m_ker_size, 3);
+	nh_priv.param<int>("train_size",  n_max,      2500);
+	nh_priv.param<std::string>("train_path",  path,  "");
+	nh_priv.param<std::string>("train_delim", delim, ",");
+
+	// Load training data from a CSV file.
+	std::fstream stream(path.c_str(), std::ios::in);
+	cv::Mat features, labels;
+	if (!stream.good()) {
+		NODELET_ERROR("unable to open training data");
+		return;
+	}
+
+	bool valid = Parse(stream, features, labels, delim[0]);
+	stream.close();
+	if (!valid) {
+		NODELET_ERROR("training data is invalid");
+		return;
+	}
+	NODELET_INFO("loaded %d training points", features.rows);
+
+	// Limit the amount of training data so OpenCV doesn't crash.
+	int n = features.rows;
+	if (n > n_max){
+		features = features(cv::Range(0, n_max), cv::Range::all());
+		labels   = labels(cv::Range(0, n_max), cv::Range::all());
+		NODELET_WARN("using %d of %d training points", n_max, n);
+	}
+
+	// Train the random forest.
+	CvMat old_features = features;
+	CvMat old_labels   = labels;
+	CvSVMParams params;
+	params.svm_type    = cv::SVM::C_SVC;
+	params.kernel_type = cv::SVM::RBF;
+	params.term_crit.epsilon  = 1e-2;
+	params.term_crit.max_iter = 25;
+	params.term_crit.type     = CV_TERMCRIT_ITER | CV_TERMCRIT_EPS;
+	m_ml.train_auto(features, labels, cv::Mat(), cv::Mat(), params);
+	NODELET_INFO("trained SVM with %d points and %d support vectors",
+	             features.rows,
+	             m_ml.get_support_vector_count()
+	);
+
+	// Subscribers and publishers.
+	m_it = boost::make_shared<image_transport::ImageTransport>(nh);
+	m_pub = m_it->advertise("white", 1);
+	m_sub = m_it->subscribe("image", 1, &MLNodelet::Callback, this);
+}
+
+void MLNodelet::FilterWhite(cv::Mat bgr_8u, cv::Mat &dst)
+{
+	int rows = bgr_8u.rows;
+	int cols = bgr_8u.cols;
+
+	dst.create(rows * cols, 1, CV_32FC1);
+
+	cv::Mat bgr;
+	bgr_8u.convertTo(bgr, CV_32FC3);
+
+	// BGR-space, HSV-space
+	std::vector<cv::Mat> ch_bgr(3), ch_hsv(3);
+	cv::Mat hsv;
+	cv::cvtColor(bgr, hsv, CV_BGR2HSV);
+	cv::split(bgr, ch_bgr);
+	cv::split(hsv, ch_hsv);
+
+	// BGRHSV-space, the feature space
+	std::vector<cv::Mat> features;
+	cv::Mat bgrhsv;
+	features.insert(features.end(), ch_bgr.begin(), ch_bgr.end());
+	features.insert(features.end(), ch_hsv.begin(), ch_hsv.end());
+	cv::merge(features, bgrhsv);
+	bgrhsv = bgrhsv.reshape(1, cols * rows);
+
+	// Classify each point.
+	for (int i = 0; i < bgrhsv.rows; ++i) {
+		CvMat  feature = bgrhsv.row(i);
+		float &value   = dst.at<float>(i, 0);
+		value = m_ml.predict(&feature);
+	}
+
+	cv::Mat mono8;
+	dst = dst.reshape(1, rows);
+	cv::normalize(dst, mono8, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+	dst = mono8;
+}
+
+void MLNodelet::Callback(sensor_msgs::Image::ConstPtr const &msg_img)
+{
+	namespace enc = sensor_msgs::image_encodings;
+	cv::Mat src, dst;
+
+	// Convert the message to the OpenCV datatype without copying it.
+	try {
+		cv_bridge::CvImageConstPtr src_tmp = cv_bridge::toCvShare(msg_img, enc::BGR8);
+		src = src_tmp->image;
+	} catch (cv_bridge::Exception const &e) {
+		NODELET_WARN_THROTTLE(10, "unable to parse image message");
+		return;
+	}
+
+	// Blur to reduce the visibility of individual blades of grass.
+	cv::Mat src_blur;
+	if (m_ker_size > 1) {
+		cv::GaussianBlur(src, src_blur, cv::Size(m_ker_size, m_ker_size), 0.0);
+	} else {
+		src_blur = src;
+	}
+	FilterWhite(src_blur, dst);
+
+	// Convert the OpenCV data to an output message without copying.
+	cv_bridge::CvImage msg_white;
+	msg_white.header   = msg_img->header;
+	msg_white.encoding = enc::MONO8;
+	msg_white.image    = dst;
+	m_pub.publish(msg_white.toImageMsg());
+}
+
+}
