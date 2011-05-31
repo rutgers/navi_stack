cdfe8c28454b2b95fc655e9ba85ae4c43b54eef2
Adam Stambler <adasta@gmail.com>
2011-05-31 15:34:44 -0400

    finally added differential drive optimizer to repo.

--- diffdrive_calibrator/src/calibrator.cpp
+++ diffdrive_calibrator/src/calibrator.cpp
@@ -3,55 +3,24 @@
 
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
-
 #include <boost/foreach.hpp>
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <navi_driver/Encoder.h>
 #include <geometry_msgs/Pose2D.h>
-#include <vector>
+#include <diffdrive_calibrator/CalibData.h>
 
+#include <vector>
 #include <string>
 
-#include <math.h>
+#include <cmath>
 
 #define PI 3.14159265
 
 
-typedef struct data_entry{
-	std::vector<navi_driver::Encoder> encoder;
-	geometry_msgs::Pose2D true_pose;
-} data_entry;
+using namespace diffdrive_calibrator;
 
-
-/*
-void readData(std::string file, std::string enc_topic, std::string pose_topic, std::vector<navi_driver::EncoderConstPtr>& data_encoder,
-								std::vector<geometry_msgs::Pose2d>& data_pose){
-
-	rosbag::Bag bag;
-	bag.open(file.c_str(), rosbag::bagmode::Read);
-
-	std::vector<std::string> topics;
-	topics.push_back(enc_topic);
-	topics.push_back(pose_topic);
-
-	rosbag::View view(bag, rosbag::TopicQuery(topics));
-
-	BOOST_FOREACH(rosbag::MessageInstance const m, view)
-	{
-		if (m.getTopic() == enc_topic){
-			data_encoder.push_back(m.instantiate<navi_driver::Encoder>() );
-		}
-		if (m.getTopic() == laser_topic) {
-			data_laser.push_back(m.instantiate<geometry_msgs::Pose2D>());
-		}
-	}
-
-	bag.close();
-}
-
-*/
 
 
 struct diff_model{
@@ -66,153 +35,160 @@ struct diff_model{
  */
 void modelUpdate(const diff_model& model, const int lw, const int rw, Eigen::Vector3d& pose){
 
-	double ld = lw*model.lw_encoder_per_meter;
-	double rd = rw *model.rw_encoder_per_meter;
+	double ld = ( (double)lw) /model.lw_encoder_per_meter;
+	double rd = ( (double )rw ) /model.rw_encoder_per_meter;
 	double ddist = (ld+rd)/2;
 	double dtheta = (rd-ld)/model.drive_diameter;
-	pose[0] += ddist* cos(pose[2]);
-	pose[1] += ddist * sin(pose[2]);
-	pose[2] += dtheta;
-	pose[2] = fmod(pose[2],2*PI);
+
+	//std::cout << "Update is " << ddist << "   " << dtheta <<std::endl;
+	pose(0) += ddist* cos(pose(2));
+	pose(1) += ddist * sin(pose(2));
+	pose(2) += dtheta;
+	//pose(2) = fmod(pose(2),2*PI);
 }
 
 
-Eigen::Vector3d integrateModel(diff_model model,const  std::vector<navi_driver::Encoder>& enc_msgs){
-
-	Eigen::Vector3d pose;
-	for(int i=0; i<enc_msgs.size(); i++){
+void integrateModel(const diff_model& model,const  std::vector<navi_driver::Encoder>& enc_msgs, Eigen::Vector3d& pose){
+	//std::cout << "Integrating model with " << enc_msgs.size() << " encoder msgs\n";
+	for(uint i=0; i<enc_msgs.size(); i++){
 		int left =enc_msgs[i].left;
 		int right =enc_msgs[i].right;
 		modelUpdate(model, left, right, pose);
+		//std::cout << "The pose is " << pose.transpose() << std::endl;
 	}
-	return pose;
 }
 
-Eigen::VectorXd computeErrors(const std::vector<data_entry>& data, const diff_model& model){
+int computeErrors(const std::vector<CalibData>& data, const diff_model& model, Eigen::VectorXd& error){
 
-	Eigen::VectorXd error(data.size());
+	error.resize(data.size());
+	int good=0;
 
 	for (uint i=0; i<data.size(); i++){
-		Eigen::Vector3d enc_pose = integrateModel(model,data[i].encoder);
+		Eigen::Vector3d enc_pose;
+		enc_pose << 0,0,0;
+
+		integrateModel(model,data[i].encoder, enc_pose);
 		Eigen::Vector3d true_pose;
-		true_pose << data[i].true_pose.x,  data[i].true_pose.y, data[i].true_pose.theta;
+		true_pose << -data[i].true_pose.x,  -data[i].true_pose.y, data[i].true_pose.theta;
+	//	std::cout << "Encoder Pose : " << enc_pose.transpose() << std::endl;
+	//	std::cout << "True Pose " << true_pose.transpose() << std::endl;
+
 		Eigen::Vector3d e = (true_pose-enc_pose);
 		Eigen::Vector2d xy; xy << e[0], e[1];
-		error(i) = xy.norm() + e[2]; //Error is simply the sum of the distance betwen poses
+		if (enc_pose.norm() > 1){
+			error(i) = e.mean(); //Error is simply the sum of the distance betwen poses
+			good++;
+		}
+		else error(i)= 0;
 	}
+
+	return good;
 }
 
 
-void computeGradient( double diff_width, const std::vector<data_entry>& data, const diff_model & m,
-					 Eigen::Vector3d& gradient){
+void computeGradient( double diff_width, const std::vector<CalibData>& data, const diff_model & m,
+					 Eigen::Vector3d& gradient, Eigen::VectorXd& c_error){
 
 	diff_model c_m= m;
 	Eigen::VectorXd f_error(data.size()), b_error(data.size());
 
+	int valid_encs  = 	computeErrors(data,m,c_error);
+
+	diff_width = 50;
 	/*** Compute Derivate around lwE ***/
 	c_m.lw_encoder_per_meter += diff_width;
-	f_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, f_error);
+	//std::cout << "LW Ferror :\n "<<f_error << std::endl;
 	c_m.lw_encoder_per_meter -= 2*diff_width;
-	b_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, b_error );
+	//std::cout << "LW berror :\n "<<b_error << std::endl;
 
-	gradient(0) = (f_error-b_error).sum()/(2*diff_width)*computeErrors(data,m);
+	std::cout << "The current error is " << c_error.transpose() << std::endl;
+	gradient(0) = (f_error-b_error).sum()/valid_encs/(2*diff_width)*c_error.sum()/valid_encs;
 
 
 
 	/*** Compute Derivative around rwE ***/
 	c_m= m;
 	c_m.rw_encoder_per_meter += diff_width;
-	f_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, f_error);
 	c_m.rw_encoder_per_meter -= 2*diff_width;
-	b_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, b_error);
 
-	gradient(1) = (f_error-b_error).sum()/(2*diff_width)* computeErrors(data,m);
+	computeErrors(data,m,c_error);
+	gradient(1) = (f_error-b_error).sum()/valid_encs/(2*diff_width)*c_error.sum()/valid_encs;
 
+	diff_width = 0.05;
 	/*** Compute Derivative around dd ***/
 	c_m= m;
 	c_m.drive_diameter += diff_width;
-	f_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, f_error);
 	c_m.drive_diameter -= 2*diff_width;
-	b_error = computeErrors(data,c_m);
+	computeErrors(data,c_m, b_error);
 
-	gradient(2) = (f_error-b_error).sum()/(2*diff_width)*computeErrors(data,m);
+	computeErrors(data,m,c_error);
+	gradient(2) = (f_error-b_error).sum()/valid_encs/(2*diff_width)*c_error.sum()/valid_encs;
 }
-
-
-
- class Listener{
-
-private:
-
-	data_entry data;
-
-	bool started;
-	geometry_msgs::Pose2D start_pose;
-	geometry_msgs::Pose2D c_pose;
-
-
-	ros::Subscriber sub_enc;
-	ros::Subscriber sub_pose;
-
-public:
-
-	Listener(ros::NodeHandle* n){
-		started = false;
-		this->sub_enc = n->subscribe("drive/encoder", 1, &Listener::encoder_cb, this);
-	}
-	~Listener(){}
-
-
-	void encoder_cb(const navi_driver::EncoderConstPtr& enc_msg){
-		data.encoder.push_back(*(enc_msg));
-	}
-
-	void resetCollection(){
-		data.encoder.clear();
-		started = false;
-	}
-
-	void pose_cb(const geometry_msgs::Pose2DConstPtr& pose_msg){
-		if (! started){
-			started= true;
-			this->start_pose = *pose_msg;
-		}
-		else{
-			this->c_pose =*pose_msg;
-		}
-	}
-
-	data_entry getData(){
-		this->data.true_pose.x =  start_pose.x-c_pose.x;
-		this->data.true_pose.y =  start_pose.y-c_pose.y;
-		this->data.true_pose.theta =  start_pose.theta-c_pose.theta;
-		return this->data;
-	}
-};
-
 
 int main(int argc, char** argv){
-	ros::init(argc,argv,"node");
 
-	/***** Collect the data  ****/
-	ros::NodeHandle n;
-	Listener lis(&n);
 
-	std::vector<data_entry> data;
-	data.resize(10);
-	double data_duration =2;
+	rosbag::Bag bag;
+	bag.open("calib_data.bag", rosbag::bagmode::Read);
 
-	for(uint i=0; i<data.size(); i++){
-		ros::Time start = ros::Time::now();
-		ros::Rate r(0.01);
-		while( (ros::Time::now()-start).sec < data_duration){
-			ros::spinOnce();
-			r.sleep();
-		}
-	data[i]=lis.getData();
-	lis.resetCollection();
+	std::vector<std::string> topics;
+	topics.push_back(std::string("data"));
+
+	std::vector<CalibData> data;
+
+	rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+	BOOST_FOREACH(rosbag::MessageInstance const m, view)
+	    {
+	        CalibData::ConstPtr msg = m.instantiate<CalibData>();
+	        if (msg != NULL)
+	        	data.push_back(*msg);
+	}
+	bag.close();
+
+	std::cout << "Data is loaded\n";
+
+
+	//************** DEFINE MODEL GUESS **********/
+	diff_model model;
+	model.lw_encoder_per_meter = 975.315;
+	model.rw_encoder_per_meter = 969.15;
+	model.drive_diameter =  0.63;
+/*
+ * 970.717  rwE  974.625  DD 0.6685
+ */
+
+
+	Eigen::Vector3d gradient;
+	Eigen::VectorXd c_error;
+	int iters = 1000;
+	for(int i =0; i < iters; i++){
+		computeGradient(0.05,data,model,gradient,c_error);
+		std::cout << "\nStep " << i << ": \n";
+		std::cout << "The Model is  lwE " << model.lw_encoder_per_meter << "  rwE  " << model.rw_encoder_per_meter << "  DD "  << model.drive_diameter << std::endl;
+		std::cout << "The average error is " << c_error.mean() << std::endl;
+		std::cout << " The Gradient is :" << gradient.transpose() << "\n";
+		double decay = exp(-((float)i)/((float)iters)/2);
+		model.lw_encoder_per_meter = model.lw_encoder_per_meter - 10*decay*gradient(0);
+		model.rw_encoder_per_meter = model.rw_encoder_per_meter -10*decay* gradient(1);
+		model.drive_diameter= model.drive_diameter - decay*gradient(2);
 	}
 
+	std::cout << "\n\nThe final Model is  lwE " << model.lw_encoder_per_meter << "  rwE  " << model.rw_encoder_per_meter << "  DD "  << model.drive_diameter << std::endl;
 
-	/*** Perform   Calibration **********/
+/*
+	Eigen::Vector3d p;
+	p<< 0,0,0;
+	modelUpdate(model, -950,950, p);
+	modelUpdate(model, -950,950, p);
+	modelUpdate(model, 950,950, p);
+	std::cout << "The pose is " << p.transpose() << std::endl;
+*/
 }
+
+
