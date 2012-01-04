5ef95a2e0711958754e74b04cd92a273ea57fa8b
Michael Koval <koval.michael@gmail.com>
2012-01-03 22:33:09 -0500

    navi_jackal: added pub/subs to skeleton

---
+++ navi_jackal/src/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * rosserial Publisher Example
+ * Prints "hello world!"
+ */
+
+#include <WProgram.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+#include "main.hpp"
+
+static std_msgs::Float32 msg_angvel;
+
+ros::NodeHandle nh;
+ros::Publisher                     pub_angvel("angvel", &msg_angvel);
+ros::Subscriber<std_msgs::Float32> sub_angvel_setp("angvel_setp", &change_setpt);
+
+void change_setpt(std_msgs::Float32 const &msg)
+{
+}
+
+void setup(void)
+{
+	nh.initNode();
+	nh.advertise(pub_angvel);
+	nh.subscribe(sub_angvel_setp);
+}
+
+void loop(void)
+{
+	nh.spinOnce();
+	delay(1000);
+}
---
+++ navi_jackal/src/main.hpp
@@ -0,0 +1,9 @@
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+void change_setpt(std_msgs::Float32 const &msg);
+
+void setup(void);
+void loop(void);
+
+#endif
