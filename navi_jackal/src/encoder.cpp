2e5444e9882ebeaf2573aaa437e77a237455874e
Michael Koval <koval.michael@gmail.com>
2012-01-04 19:15:07 -0500

    navi_jackal: It works. One wire was broken. :(

--- navi_jackal/src/encoder.cpp
+++ navi_jackal/src/encoder.cpp
@@ -1,14 +1,13 @@
 #include <WProgram.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include "config.hpp"
 #include "encoder.hpp"
 
-static volatile bool last_enc1a = 0;
-static volatile bool last_enc1b = 0;
-static volatile bool last_enc2a = 0;
-static volatile bool last_enc2b = 0;
+static volatile uint8_t last_enc1a = 0;
+static volatile uint8_t last_enc1b = 0;
+static volatile uint8_t last_enc2a = 0;
+static volatile uint8_t last_enc2b = 0;
 
 volatile int16_t motor1_ticks = 0;
 volatile int16_t motor2_ticks = 0;
@@ -16,10 +15,10 @@ volatile int16_t motor2_ticks = 0;
 void encoder_init(void)
 {
 	// Set pins as inputs (DDRD = 0) with an internal pull-up (PORTD = 1).
-	DDRD  &= ~(1 << MOTOR1_ENCA | 1 << MOTOR1_ENCB
-	         | 1 << MOTOR2_ENCA | 1 << MOTOR2_ENCB);
-	PORTD |=   1 << MOTOR1_ENCA | 1 << MOTOR1_ENCB
-	         | 1 << MOTOR2_ENCA | 1 << MOTOR2_ENCB;
+	DDRD  &= ~(1 << PIN2 | 1 << PIN3
+	         | 1 << PIN4 | 1 << PIN5);
+	PORTD |=   1 << PIN2 | 1 << PIN3
+	         | 1 << PIN4 | 1 << PIN5;
 
 	// Enable pin-change interrupts.
 	cli();
@@ -28,31 +27,44 @@ void encoder_init(void)
 	sei();
 }
 
-static inline int8_t encoder_tick(bool last_a, bool last_b,
-                                  bool curr_a, bool curr_b)
+static inline int8_t encoder_tick(uint8_t last_a, uint8_t last_b,
+                                  uint8_t curr_a, uint8_t curr_b,
+								  volatile int16_t *ticks)
 {
 	if (!last_a && curr_a) {
-		return (!curr_b) ? -1 : +1;
+		if (!curr_b)
+			*ticks -= 1;
+		else
+			*ticks += 1;
 	} else if (last_a && !curr_a) {
-		return (!curr_b) ? +1 : -1;
+		if (!curr_b)
+			*ticks += 1;
+		else
+			*ticks -= 1;
 	}
 
 	if (!last_b && curr_b) {
-		return (!curr_a) ? +1 : -1;
+		if (!curr_a)
+			*ticks += 1;
+		else
+			*ticks -= 1;
 	} else if (last_b && !curr_b) {
-		return (!curr_a) ? -1 : +1;
+		if (!curr_a)
+			*ticks -= 1;
+		else
+			*ticks += 1;
 	}
 }
 
 ISR(PCINT2_vect)
 {
-	bool const enc1a = !!(PIND & 1 << MOTOR1_ENCA);
-	bool const enc1b = !!(PIND & 1 << MOTOR1_ENCB);
-	bool const enc2a = !!(PIND & 1 << MOTOR2_ENCA);
-	bool const enc2b = !!(PIND & 1 << MOTOR2_ENCB);
+	uint8_t const enc1a = !!(PIND & 1 << PIN2);
+	uint8_t const enc1b = !!(PIND & 1 << PIN3);
+	uint8_t const enc2a = !!(PIND & 1 << PIN4);
+	uint8_t const enc2b = !!(PIND & 1 << PIN5);
 
-	motor1_ticks += encoder_tick(last_enc1a, last_enc1b, enc1a, enc1b);
-	motor2_ticks += encoder_tick(last_enc2a, last_enc2b, enc2a, enc2b);
+	encoder_tick(last_enc1a, last_enc1b, enc1a, enc1b, &motor1_ticks);
+	encoder_tick(last_enc2a, last_enc2b, enc2a, enc2b, &motor2_ticks);
 
 	last_enc1a = enc1a;
 	last_enc1b = enc1b;
