5a9a31c8e239cd76a1efc2957080c322529cb03a
Michael Koval <koval.michael@gmail.com>
2012-01-19 03:17:59 -0500

    navi_jackal: Updated calibration script; untested.

--- navi_jackal/nodes/calibrate.py
+++ navi_jackal/nodes/calibrate.py
@@ -4,70 +4,92 @@ from __future__ import print_function
 
 import roslib; roslib.load_manifest('navi_jackal')
 import rospy
-from navi_jackal.msg import EncoderTicks, VoltageSetpoint
+import csv
+import sys
+
+from navi_jackal.srv import CalibrationSetpoint
 
 class Calibrator:
     def __init__(self, dur_delay, dur_sample):
-        self.pub_pwm = rospy.Publisher('voltages', VoltageSetpoint)
-        self.sub_enc = rospy.Subscriber('encoders', EncoderTicks, self.encoders_tick)
+        self.srv_pwm = rospy.ServiceProxy('calibrate', CalibrationSetpoint)
         self.ticks_left  = 0
         self.ticks_right = 0
 
         self.pid_freq = rospy.get_param('~pid_frequency')
-        self.pwm_max  = rospy.get_param('~pwm_max')
 
         self.dur_delay  = dur_delay
         self.dur_sample = dur_sample
         self.table_left  = list()
         self.table_right = list()
 
-    def encoders_tick(self, encoders):
-        self.ticks_left  += encoders.ticks_left
-        self.ticks_right += encoders.ticks_right
-
-    def encoders_clear(self):
-        self.ticks_left  = 0
-        self.ticks_right = 0
+    def set_pwm(self, pwm):
+        (ticks_left, ticks_right) = self.srv_pwm(pwm, pwm)
+        omega_left  = self.scale_omega(ticks_left)
+        omega_right = self.scale_omega(ticks_right)
+        return (omega_left, omega_right)
 
     def scale_omega(self, ticks):
         return ticks / (self.dur_sample * self.pid_freq)
 
-    def evaluate_pwm(self, pwm):
-        self.pub_pwm.publish(pwm_left = pwm, pwm_right = pwm)
+    def evaluate(self, pwms):
+        table = list()
+        pwm = pwms[0]
 
-        rospy.sleep(self.dur_delay)
-        self.encoders_clear()
-        rospy.sleep(self.dur_sample)
+        rospy.wait_for_service('calibrate')
 
-        omega_left  = self.scale_omega(self.ticks_left)
-        omega_right = self.scale_omega(self.ticks_right)
+        for pwm_next in pwms[1:] + [ 0 ]:
+            # Setting the same speed twice is intentional because it clears the
+            # current count of encoder ticks. This delay gives the motor time
+            # to reach its steady-state velocity.
+            self.set_pwm(pwm)
+            rospy.sleep(dur_delay)
+            self.set_pwm(pwm)
 
-        self.table_left.append(omega_left)
-        self.table_right.append(omega_right)
+            # Calculate the average number of ticks per PID loop over the
+            # sample duration.
+            rospy.sleep(dur_sample)
+            (ticks_left, ticks_right) = self.set_pwm(pwm_next)
+            omega_left  = self.scale_omega(ticks_left)
+            omega_right = self.scale_omega(ticks_right)
 
-    def stop(self):
-        self.pub_pwm.publish(pwm_left = 0, pwm_right = 0)
+            table.append((pwm, omega_left, omega_right))
+            pwm_curr = pwm
+
+        return table
 
 def main():
     rospy.init_node('calibrate')
 
     try:
-        (_, dur_delay, dur_sample, pwm_step) = rospy.myargv()
-    except ValueError:
+        args = rospy.myargv()
+        dur_delay  = float(args[1])
+        dur_sample = float(args[2])
+        pwm_step   = int(args[3])
+    except IndexError:
         rospy.logerr('Incorrect number of arguments.')
         rospy.logerr('usage: ./step.py <delay> <sample> <pwm step>')
         rospy.signal_shutdown('Incorrect number of arguments.')
-        return
+        return 1
+    except ValueError:
+        rospy.logerr('One or more arguments has an invalid type.')
+        rospy.signal_shutdown('One or more arguments has an invalid type.')
+        return 1
 
-    node = Calibrator(float(dur_delay), float(dur_sample))
-    for pwm in range(-node.pwm_max, node.pwm_max + 1, int(pwm_step)):
-        node.evaluate_pwm(pwm)
-    node.stop()
+    pwm_max = rospy.get_param('~pwm_max')
+    pwms    = range(-pwm_max, pwm_max + 1, pwm_step)
 
-    print('Left:  ', node.table_left)
-    print('Right: ', node.table_right)
+    # Generate a three-column table of left and right angular velocity versus
+    # input PWM value (i.e. voltage).
+    node   = Calibrator(dur_delay, dur_sample)
+    table  = node.evaluate(pwms)
+    writer = csv.writer(sys.stdin)
+    writer.writerows(table)
+    return 0
 
 if __name__ == '__main__':
-    main()
+    try:
+        sys.exit(main())
+    except rospy.ROSInterruptException:
+        sys.exit(1)
 
 # vim: set et:set sw=4 ts=4:
