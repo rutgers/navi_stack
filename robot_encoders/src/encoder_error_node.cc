ac098efabaf1748e0f1991bb323c5b7889d0e488
Michael Koval <koval.michael@gmail.com>
2012-04-16 01:39:59 -0400

    switched to ROS parameters

--- robot_encoders/src/encoder_error_node.cc
+++ robot_encoders/src/encoder_error_node.cc
@@ -21,10 +21,24 @@ static ros::Publisher pub_odom;
 static Eigen::Vector3d last_pos, last_noisy_pos;
 static double last_angle, last_noisy_angle;
 
+static int ticks_per_rev;
 static double robot_radius;
+static double wheel_radius;
 static double alpha;
 static double const min_variance = 1e-6;
 static RNGType rng;
+
+double add_discretization_error(double distance, int ticks_per_rev)
+{
+    // TODO: Figure out why this discretization error doesn't work.
+#if 0
+    double const revs = distance / 2 * M_PI * wheel_radius;
+    int const ticks = round(revs * ticks_per_rev);
+    double const measured_revs = static_cast<double>(ticks) / ticks_per_rev;
+    return measured_revs * 2 * M_PI * wheel_radius;
+#endif
+    return distance;
+}
 
 void updateOdom(nav_msgs::Odometry const &msg_in)
 {
@@ -43,22 +57,24 @@ void updateOdom(nav_msgs::Odometry const &msg_in)
     double const delta_angle  = angles::normalize_angle(curr_angle - last_angle);
 
     // Convert from polar coordinates to encoder ticks.
-    double const ticks_left  = delta_linear - 0.5 * robot_radius * delta_angle;
-    double const ticks_right = delta_linear + 0.5 * robot_radius * delta_angle;
+    double const exact_v_left  = delta_linear - 0.5 * robot_radius * delta_angle;
+    double const exact_v_right = delta_linear + 0.5 * robot_radius * delta_angle;
+    double const v_left  = add_discretization_error(exact_v_left, ticks_per_rev);
+    double const v_right = add_discretization_error(exact_v_right, ticks_per_rev);
 
     // Add noise to the encoder ticks.
-    double const sigma_left  = std::max(alpha * ticks_left,  min_variance);
-    double const sigma_right = std::max(alpha * ticks_right, min_variance);
+    double const sigma_left  = std::max(alpha * v_left,  min_variance);
+    double const sigma_right = std::max(alpha * v_right, min_variance);
     normal_dist const dist_left(0.0, sigma_left);
     normal_dist const dist_right(0.0, sigma_right);
     boost::variate_generator<RNGType, normal_dist> noise_left(rng, dist_left);
     boost::variate_generator<RNGType, normal_dist> noise_right(rng, dist_right);
-    double const noisy_ticks_left  = ticks_left + noise_left();
-    double const noisy_ticks_right = ticks_right + noise_right();
+    double const noisy_v_left  = v_left + noise_left();
+    double const noisy_v_right = v_right + noise_right();
 
     // Convert back from encoder ticks to polar coordinates.
-    double const noisy_delta_linear = 0.5 * (noisy_ticks_left + noisy_ticks_right);
-    double const noisy_delta_angle  = (noisy_ticks_right - noisy_ticks_left) / robot_radius;
+    double const noisy_delta_linear = 0.5 * (noisy_v_left + noisy_v_right);
+    double const noisy_delta_angle  = (noisy_v_right - noisy_v_left) / robot_radius;
 
     // Convert back from polar coordinates to cartaesian coordinates.
     double const noisy_angle = angles::normalize_angle(last_noisy_angle + noisy_delta_angle);
@@ -81,23 +97,15 @@ void updateOdom(nav_msgs::Odometry const &msg_in)
     msg_out.twist.covariance[0] = -1;
 
     // TODO: Propagate the variances through the transformation.
-#if 0
-    double const var_x = sigma_linear * sigma_linear * cos(last_angle);
-    double const var_y = sigma_linear * sigma_linear * sin(last_angle);
-    double const var_angle = sigma_angular * sigma_angular;
-#else
-    double const var_x = 0.0;
-    double const var_y = 0.0;
-    double const var_angle = 0.0;
-#endif
+    double const var = 0.5 * (pow(sigma_left, 2) + pow(sigma_right, 2));
     double const big = 99999;
     Eigen::Map<Eigen::Matrix<double, 6, 6> > cov_raw(&msg_out.pose.covariance[0]);
-    cov_raw << var_x,   0.0, 0.0, 0.0, 0.0, 0.0,
-                 0.0, var_y, 0.0, 0.0, 0.0, 0.0,
-                 0.0,   0.0, big, 0.0, 0.0, 0.0,
-                 0.0,   0.0, 0.0, big, 0.0, 0.0,
-                 0.0,   0.0, 0.0, 0.0, big, 0.0,
-                 0.0,   0.0, 0.0, 0.0, 0.0, var_angle;
+    cov_raw << var, 0.0, 0.0, 0.0, 0.0, 0.0,
+               0.0, var, 0.0, 0.0, 0.0, 0.0,
+               0.0, 0.0, big, 0.0, 0.0, 0.0,
+               0.0, 0.0, 0.0, big, 0.0, 0.0,
+               0.0, 0.0, 0.0, 0.0, big, 0.0,
+               0.0, 0.0, 0.0, 0.0, 0.0, var;
 
     // TODO: Also publish a TF transform.
     pub_odom.publish(msg_out);
@@ -111,15 +119,17 @@ void updateOdom(nav_msgs::Odometry const &msg_in)
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "encoder_error_node");
-
     last_pos << 0.0, 0.0, 0.0;
     last_angle =  0.0;
 
-    alpha = 0.1;
-    robot_radius = 0.3;
-    pub_tf = boost::make_shared<tf::TransformBroadcaster>();
 
     ros::NodeHandle nh;
+    ros::param::get("~alpha", alpha);
+    ros::param::get("~robot_radius", robot_radius);
+    ros::param::get("~wheel_radius", wheel_radius);
+    ros::param::get("~ticks_per_rev", ticks_per_rev);
+
+    pub_tf = boost::make_shared<tf::TransformBroadcaster>();
     sub_odom = nh.subscribe("odom_true", 1, &updateOdom);
     pub_odom = nh.advertise<nav_msgs::Odometry>("odom", 10);
 
