b3e819dc962ee094a5342d6bbd5f9fdca2721666
Michael Koval <koval.michael@gmail.com>
2012-05-26 01:14:45 -0400

    switched to actionlib for setting goals

--- navi_executive/src/executive.cpp
+++ navi_executive/src/executive.cpp
@@ -3,11 +3,16 @@
 #include <string>
 #include <stdint.h>
 #include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <nav_msgs/Odometry.h>
 #include <navi_executive/executive.h>
 #include <navi_executive/AddWaypoint.h>
 #include <navi_executive/Waypoint.h>
 
-using namespace navi_executive;
+using move_base_msgs::MoveBaseAction;
+using navi_executive::AddWaypoint;
+using navi_executive::Waypoint;
 
 bool addWaypointCallback(AddWaypoint::Request &request,
                          AddWaypoint::Response &response)
@@ -21,8 +26,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "executive");
     ros::NodeHandle nh;
 
+    actionlib::SimpleActionClient<MoveBaseAction> act_goal("move_base/goal", true);
     ros::ServiceServer srv_add = nh.advertiseService("add_waypoint", &addWaypointCallback);
 
-    ros::spin();
+    act_goal.waitForServer();
     return 0;
 }
