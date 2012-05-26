836c71788c8d1e2d8ebf78059c743adc00e65977
Michael Koval <koval.michael@gmail.com>
2012-05-26 00:24:33 -0400

    split rosparam code into a separate node

--- navi_executive/src/executive.cpp
+++ navi_executive/src/executive.cpp
@@ -6,77 +6,9 @@
 #include <XmlRpcValue.h>
 #include <navi_executive/executive.h>
 
-using navi_executive::Waypoint;
-
-namespace navi_executive {
-};
-
-static void checkParamType(XmlRpc::XmlRpcValue value, int type,
-                           std::string const &error_msg)
-{
-    if (value.getType() != type) {
-        ROS_FATAL("Error parsing waypoint list: %s", error_msg.c_str());
-    }
-}
-
-static void parseWaypoint(XmlRpc::XmlRpcValue coords, Waypoint &waypoint)
-{
-    checkParamType(coords, XmlRpc::XmlRpcValue::TypeArray,
-                   "Waypoint must be a list of two doubles.");
-    if (coords.size() != 2) {
-        ROS_FATAL("Waypoint must be a list of two doubles.");
-    }
-
-    checkParamType(coords[0], XmlRpc::XmlRpcValue::TypeDouble,
-                   "Latitude must be a real number.");
-    checkParamType(coords[1], XmlRpc::XmlRpcValue::TypeDouble,
-                   "Longitude must be a real number.");
-    waypoint.lat = static_cast<double>(coords[0]);
-    waypoint.lon = static_cast<double>(coords[1]);
-}
-
-static void parseWaypointList(XmlRpc::XmlRpcValue waypoint_list,
-                              std::vector<Waypoint> &waypoints)
-{
-    checkParamType(waypoint_list, XmlRpc::XmlRpcValue::TypeArray,
-                   "Waypoint group must be a list of waypoints.");
-
-    waypoints.resize(waypoint_list.size());
-    for (int32_t i = 0; i < waypoint_list.size(); ++i) {
-        parseWaypoint(waypoint_list[i], waypoints[i]);
-    }
-}
-
-static void parseGroupList(XmlRpc::XmlRpcValue group_list,
-                           std::vector<std::vector<Waypoint> > &groups)
-{
-    checkParamType(group_list, XmlRpc::XmlRpcValue::TypeArray,
-                   "Expected a list of waypoint groups.");
-
-    groups.resize(group_list.size());
-    for (int32_t i = 0; i < group_list.size(); ++i) {
-        parseWaypointList(group_list[i], groups[i]);
-    }
-}
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "executive");
     ros::NodeHandle nh;
-
-    XmlRpc::XmlRpcValue waypoint_list;
-    nh.getParam("waypoints", waypoint_list);
-
-    std::vector<std::vector<Waypoint> > waypoints;
-    parseGroupList(waypoint_list, waypoints);
-
-    for (size_t i = 0; i < waypoints.size(); ++i) {
-        std::cout << "Group #" << i << ":\n";
-
-        for (size_t j = 0; j < waypoints[i].size(); ++j) {
-            std::cout << "- (" << waypoints[i][j].lat
-                      << ", "  << waypoints[i][j].lon << ")\n";
-        }
-    }
     return 0;
 }
---
+++ navi_executive/src/waypoint_loader.cpp
@@ -0,0 +1,90 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdint.h>
+#include <ros/ros.h>
+#include <XmlRpcValue.h>
+#include <navi_executive/executive.h>
+#include <navi_executive/AddWaypoint.h>
+#include <navi_executive/Waypoint.h>
+
+using namespace navi_executive;
+
+static void checkParamType(XmlRpc::XmlRpcValue value, int type,
+                           std::string const &error_msg)
+{
+    if (value.getType() != type) {
+        ROS_FATAL("Error parsing waypoint list: %s", error_msg.c_str());
+    }
+}
+
+static void parseWaypoint(XmlRpc::XmlRpcValue coords, Waypoint &waypoint)
+{
+    checkParamType(coords, XmlRpc::XmlRpcValue::TypeArray,
+                   "Waypoint must be a list of two doubles.");
+    if (coords.size() != 2) {
+        ROS_FATAL("Waypoint must be a list of two doubles.");
+    }
+
+    checkParamType(coords[0], XmlRpc::XmlRpcValue::TypeDouble,
+                   "Latitude must be a real number.");
+    checkParamType(coords[1], XmlRpc::XmlRpcValue::TypeDouble,
+                   "Longitude must be a real number.");
+    waypoint.lat = static_cast<double>(coords[0]);
+    waypoint.lon = static_cast<double>(coords[1]);
+}
+
+static void parseWaypointList(XmlRpc::XmlRpcValue waypoint_list,
+                              std::vector<Waypoint> &waypoints)
+{
+    checkParamType(waypoint_list, XmlRpc::XmlRpcValue::TypeArray,
+                   "Waypoint group must be a list of waypoints.");
+
+    waypoints.resize(waypoint_list.size());
+    for (int32_t i = 0; i < waypoint_list.size(); ++i) {
+        parseWaypoint(waypoint_list[i], waypoints[i]);
+    }
+}
+
+static void parseGroupList(XmlRpc::XmlRpcValue group_list,
+                           std::vector<std::vector<Waypoint> > &groups)
+{
+    checkParamType(group_list, XmlRpc::XmlRpcValue::TypeArray,
+                   "Expected a list of waypoint groups.");
+
+    groups.resize(group_list.size());
+    for (int32_t i = 0; i < group_list.size(); ++i) {
+        parseWaypointList(group_list[i], groups[i]);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "waypoint_loader", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+
+    // Load a list of waypoints from the parameter server.
+    XmlRpc::XmlRpcValue waypoint_list;
+    std::vector<std::vector<Waypoint> > waypoints;
+    nh.getParam("waypoints", waypoint_list);
+    parseGroupList(waypoint_list, waypoints);
+
+    // Iteratively add the waypoints to the executive.
+    ros::ServiceClient srv = nh.serviceClient<AddWaypoint>("add_waypoint");
+    srv.waitForExistence();
+
+    for (size_t i = 0; i < waypoints.size(); ++i) {
+        AddWaypoint::Request srv_request;
+        AddWaypoint::Response srv_response;
+        srv_request.waypoints = waypoints[i];
+        srv.call(srv_request, srv_response);
+
+        std::stringstream ss;
+        for (size_t j = 0; j < waypoints[i].size(); ++j) {
+            ss << " (" << waypoints[i][j].lat << ", " << waypoints[i][j].lon << ")";
+        }
+
+        ROS_INFO("Added Group #%d:%s", static_cast<int>(i + 1), ss.str().c_str());
+    }
+    return 0;
+}
