4da26d2328a0b82f2a505b081f106229da92b110
David Patrzeba <david.patrzeba@gmail.com>
2012-06-08 21:26:02 -0400

    add initial JAUS node

---
+++ navi_jaus/src/jaus_node.cpp
@@ -0,0 +1,294 @@
+//JAUS Headers-------------------------------------------------//
+#include <jaus/mobility/sensors/globalposesensor.h>            
+#include <jaus/mobility/sensors/localposesensor.h>             
+#include <jaus/mobility/sensors/velocitystatesensor.h>         
+#include <jaus/mobility/drivers/localwaypointlistdriver.h>     
+#include <jaus/core/transport/judp.h>                          
+#include <jaus/core/transport/jtcpclient.h>                    
+#include <jaus/core/component.h>                               
+#include <cxutils/keyboard.h>                                  
+#include <cxutils/math/cxmath.h>                               
+#include <iostream>                                            
+#include <cstring>                                             
+#include <cstdio>                                              
+#include <cmath>                                               
+/////////////////////////////////////////////////////////////////
+
+
+//ROS ---------------------------------------------------//  needed??
+#include <list>                                          //
+#include <string>                                        //
+#include <sstream>                                       //
+#include <stdint.h>                                      //
+#include <boost/lambda/lambda.hpp>                       //
+#include <ros/ros.h>                                     //
+#include <actionlib/client/simple_action_client.h>       //
+#include <gps_common/conversions.h>                      //
+#include <move_base_msgs/MoveBaseAction.h>               //
+#include <move_base_msgs/MoveBaseGoal.h>                 //
+#include <nav_msgs/Odometry.h>                           //
+#include <navi_executive/executive.h>                    //
+#include <navi_executive/AddWaypoint.h>                  //
+#include <navi_executive/WaypointGPS.h>                  //
+#include <navi_executive/WaypointUTM.h>                  //
+///////////////////////////////////////////////////////////
+
+
+//JAUS Global Variables---------------//
+JAUS::UShort gSubsystemID   = 152;    // ID of our subsystem to use.  Change to last ocet of ip address.
+JAUS::Byte gNodeID          = 1;      // ID of our node to use.
+JAUS::Byte gComponentID     = 1;      // ID of the our component.
+////////////////////////////////////////
+
+
+//ROS-------------------------------------------// needed??
+using actionlib::SimpleClientGoalState;         //
+using move_base_msgs::MoveBaseAction;           //
+using move_base_msgs::MoveBaseResultConstPtr;   //
+using move_base_msgs::MoveBaseGoal;             //
+using navi_executive::AddWaypoint;              //
+using navi_executive::WaypointGPS;              //
+using navi_executive::WaypointUTM;              //
+//////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+int main(int argc, char **argv)
+{
+    
+    //TODO: update with appropriate calls
+    ros::init(argc, argv, "jaus");                     ///////////////
+    ros::NodeHandle nh;
+      
+    //Add something to first field
+    ros::Subscriber pose_sub = nh.subscribe("", 100, callback); /////////////////
+
+    //Wait until ROS responds to it
+    std::cout << "Trying to connect to ROS...";
+	 while(!ros_init)                             
+    {                                   
+	   std::cout << "." << flush; 
+	   ros::spinOnce();       
+	   CxUtils::SleepMs(500);                                                     
+	 }   
+    std::cout << "CONNECTED!\n" << flush; 
+
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //SETUP JAUS-----------------------------------------------------------------------------------//
+                             
+    //Create JAUS component 
+    std::cout << "Starting JAUS\n";                       
+    JAUS::Component component;                   
+                                    
+    //Disable timeout                                    
+    component.AccessControlService()->SetTimeoutPeriod(0);      
+                                              
+    //Add Global Position Sensor, Local Position Sensor, Velocity Sensor to JAUS component 
+    JAUS::GlobalPoseSensor* globalPoseSensor = new JAUS::GlobalPoseSensor();
+    globalPoseSensor->SetSensorUpdateRate(25); //update @ 25Hz   
+    component.AddService(globalPoseSensor);      
+                                                      
+    JAUS::LocalPoseSensor* localPoseSensor = new JAUS::LocalPoseSensor(); 
+    localPoseSensor->SetSensorUpdateRate(25);  //update @ 25Hz  
+    component.AddService(localPoseSensor);  
+                                                
+    JAUS::VelocityStateSensor* velocityStateSensor = new JAUS::VelocityStateSensor();
+    velocityStateSensor->SetSensorUpdateRate(25);  //update @ 25Hz   
+    component.AddService(velocityStateSensor); 
+                                        
+    //List for waypoints                               
+    component.AddService(new JAUS::ListManager());           
+    JAUS::LocalWaypointListDriver* localWaypointListDriver = new JAUS::LocalWaypointListDriver();  
+    component.AddService(localWaypointListDriver);     
+                                       
+    //make discoverable
+    //TASK 1 -------------------------XXX
+    component.DiscoveryService()->SetSubsystemIdentification(JAUS::Subsystem::Vehicle, "navi");
+
+    //Initialize JAUS, all components should be added at this time
+    if(component.Initialize(JAUS::Address(gSubsystemID, gNodeID, gComponentID)) == false) 
+    {                                                                    
+        std::cout << "Failed to Initialize Component.\n";                         
+        return 0;                                                      
+    }                                                                      
+                                                                    
+    //Standby                                                    
+    component.ManagementService()->SetStatus(JAUS::Management::Status::Standby);    
+    
+    //add JDUP as service ............. JTCPClient ......JTCP
+    JAUS::JUDP* transportService = NULL;                                               
+    transportService = (JAUS::JUDP*)component.TransportService();                 
+                                                                                         
+    //       
+    transportService->AddConnection("192.168.1.42", JAUS::Address(42, 1, 1), 3794);
+      
+            
+                                                                                     
+    // Set an initial global pose.                                              
+    // TODO: pull values from GPS    
+    // TODO: pull values from kalman filter, change to global and pass to JAUS
+    JAUS::GlobalPose globalPose;                                          
+    globalPose.SetLatitude(/*Latitudecall*/);                            
+    globalPose.SetLongitude(/*longcall*/);                               
+    //globalPose.SetAltitude(0);                                       
+    globalPose.SetPositionRMS(0.0);                                         
+    //globalPose.SetRoll(0.0);                                             
+    //globalPose.SetPitch(0.0);                                           
+    //globalPose.SetYaw(/**/);     
+    globalPose.SetAttitudeRMS(0.0);                                  
+    globalPose.SetTimeStamp(JAUS::Time::GetUtcTime());          
+                                                                                                   
+    // Set an initial velocity state.                                                              
+    JAUS::VelocityState velocityState;                                                             
+    velocityState.SetVelocityX(0.0);                                                               
+    velocityState.SetYawRate(0.0);                                                                 
+    velocityState.SetVelocityRMS(0.0);                                                             
+    velocityState.SetTimeStamp(JAUS::Time::GetUtcTime());               
+        
+    //Save the data to the service. 
+    //send globalPose to localPose, conversion automatic
+    globalPoseSensor->SetGlobalPose(globalPose);                                  
+    localPoseSensor->SetLocalPose(globalPose);                                                     
+    velocityStateSensor->SetVelocityState(velocityState);                                          
+                                                                                                  
+    JAUS::Time::Stamp printTimeMs = 0;  
+
+
+    //END SETUP JAUS--------------------------------------------------------------------------------//
+
+                                                                                                                                
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    
+    
+    
+    
+    
+    //Main JAUS loop-------------------------------------------------------------------------------//
+                                                                                                   //      
+                                                                                                   //
+    while(ros::ok())
+    {
+        
+       //handle shutdown, standby, waypoint collection
+        //shutdown
+        if(component.ManagementService()->GetStatus() == JAUS::Management::Status::Shutdown)
+        {
+		    std::cout << "\nTerminating the Program. Shutdown command receviced!\n";
+
+		    break;
+		  }
+		  //If standby
+		  if(component.ManagementService()->GetStatus() == JAUS::Management::Status::Standby)
+        {
+		    std::cout << "\nStandby!\n";
+		  }
+
+
+
+        // Get local waypoint list
+		  JAUS::Element::Map elementList = localWaypointListDriver->GetElementList();
+		  // Convert to SetLocalWaypointCommands
+		  JAUS::Element::Map::iterator listElement;
+		  std::vector<JAUS::SetLocalWaypoint> commandList;
+		  for(listElement = elementList.begin(); listElement != elementList.end(); listElement++)
+	 	  {
+			 if(listElement->second.mpElement->GetMessageCode() == JAUS::SET_LOCAL_WAYPOINT)
+			 {
+				 commandList.push_back(*( (JAUS::SetLocalWaypoint *)(listElement->second.mpElement)) );
+		 	 }
+		  } 
+
+        //commandList is a vector of Waypoints
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //Execute 
+        if(localWaypointListDriver->IsExecuting())
+        {
+         // TODO: Go through this, should be replace by executive
+                                                                                
+        }
+                                                                                                   
+                                                                                                   
+                                                                                                   
+                  
+
+
+
+
+
+
+
+                                                                                                   
+        //XXX:Everything below is correct except TODO                                                                                           
+                                                                                                   
+        // Save newly calculated position and orientation.     
+        //TODO: Update collection of data with calls
+        //globalPose.SetYaw(/**/);                                                                 
+        globalPose.SetLatitude(/**/);                                      
+        globalPose.SetLongitude(/**/);                                 
+        globalPose.SetAltitude(/**/);                          
+        globalPose.SetTimeStamp(JAUS::Time(true));                                      
+                                                                                     
+        velocityState.SetVelocityX(/**/);                             
+        velocityState.SetYawRate(/**/);                           
+        velocityState.SetTimeStamp(JAUS::Time(true));                      
+                                                                            
+        // Save the data to the service                                 
+        globalPoseSensor->SetGlobalPose(globalPose);        
+        localPoseSensor->SetLocalPose(globalPose);                         
+        velocityStateSensor->SetVelocityState(velocityState);         
+                                                                                                   
+        
+        
+        
+        
+        //Update Status
+        if(JAUS::Time::GetUtcTimeMs() - printTimeMs > 5000)                                         
+        {                                                                                          
+            // Print status of services.                                                           
+            std::cout << "\n=======================Basic Service Status============================\n";             
+            component.AccessControlService()->PrintStatus(); std::cout << std::endl;               
+            component.ManagementService()->PrintStatus(); std::cout << std::endl;                  
+            //globalPoseSensor->PrintStatus(); std::cout << std::endl;                               
+            localPoseSensor->PrintStatus(); std::cout << std::endl;                                
+            velocityStateSensor->PrintStatus(); std::cout << std::endl;                            
+            localWaypointListDriver->PrintStatus();                                                
+            printTimeMs = JAUS::Time::GetUtcTimeMs();                                              
+        }                                                                                          
+                                                                                                   
+        // Exit if escape key pressed.                                                             
+        if(CxUtils::GetChar() == 27)                                                               
+        {                                                                                          
+            break;                                                                                 
+        }                                                                                          
+                                                                                                   
+        
+        CxUtils::SleepMs(250);
+	     ros::spinOnce();
+    }                                                                                              
+    //END MAIN JAUS LOOP---------------------------------------------------------------------------//
+                                                                                                   
+    // Shutdown any components associated with our subsystem.                                  
+    component.Shutdown();                                                   
+
+    return 0;
+}
